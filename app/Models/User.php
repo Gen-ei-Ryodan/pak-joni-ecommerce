2cb27bfee2914636625c07bc3629ae1187d665c6
Cristianalbertus <10969sosho@192.168.1.2>
2026-05-15 19:41:00 +0700

    fix: tambah $fillable tradisional untuk PHP 8.2

--- app/Models/User.php
+++ app/Models/User.php
@@ -19,6 +19,10 @@ class User extends Authenticatable
     /** @use HasFactory<UserFactory> */
     use HasFactory, Notifiable;
 
+    protected $fillable = ['role', 'name', 'email', 'password'];
+
+    protected $hidden = ['password', 'remember_token'];
+
     /**
      * Get the attributes that should be cast.
      *
