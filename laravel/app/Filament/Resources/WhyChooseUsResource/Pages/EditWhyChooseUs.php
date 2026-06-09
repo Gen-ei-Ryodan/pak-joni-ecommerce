bfdeb261dde33964ed50f2f0b072d381b2c434ff
Cristianalbertus <10969sosho@Cristianalbertuss-MacBook-Air.local>
2026-06-09 13:54:58 +0700

    Fix EditWhyChooseUs: preserve icon_image (deploy)

--- laravel/app/Filament/Resources/WhyChooseUsResource/Pages/EditWhyChooseUs.php
+++ laravel/app/Filament/Resources/WhyChooseUsResource/Pages/EditWhyChooseUs.php
@@ -1,1 +1,20 @@
-<?php namespace App\Filament\Resources\WhyChooseUsResource\Pages; use App\Filament\Resources\WhyChooseUsResource; use Filament\Resources\Pages\EditRecord; class EditWhyChooseUs extends EditRecord { protected static string $resource = WhyChooseUsResource::class; }
+<?php
+
+namespace App\Filament\Resources\WhyChooseUsResource\Pages;
+
+use App\Filament\Resources\WhyChooseUsResource;
+use Filament\Resources\Pages\EditRecord;
+
+class EditWhyChooseUs extends EditRecord
+{
+    protected static string $resource = WhyChooseUsResource::class;
+
+    protected function mutateFormDataBeforeSave(array $data): array
+    {
+        if (empty($data['icon_image']) && $this->record->icon_image) {
+            $data['icon_image'] = $this->record->icon_image;
+        }
+
+        return $data;
+    }
+}
