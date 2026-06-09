7e659757efc642850c38bcc0a7f458017136ab46
Cristianalbertus <10969sosho@Cristianalbertuss-MacBook-Air.local>
2026-06-09 13:17:56 +0700

    Fix banner edit: keep existing image_path on save, make image_path nullable

--- app/Filament/Resources/BannerResource/Pages/EditBanner.php
+++ app/Filament/Resources/BannerResource/Pages/EditBanner.php
@@ -10,10 +10,10 @@ class EditBanner extends EditRecord
 {
     protected static string $resource = BannerResource::class;
 
-    protected function mutateFormDataBeforeFill(array $data): array
+    protected function mutateFormDataBeforeSave(array $data): array
     {
-        if (!empty($data['image_path'])) {
-            $data['image_path'] = str_replace('banners/', '', $data['image_path']);
+        if (empty($data['image_path']) && $this->record->image_path) {
+            $data['image_path'] = $this->record->image_path;
         }
 
         return $data;
