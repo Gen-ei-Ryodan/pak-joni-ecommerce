269442dca97d85dc7988284729723db0ce00d12f
Cristianalbertus <10969sosho@Cristianalbertuss-MacBook-Air.local>
2026-07-13 09:31:59 +0700

    fix: add missing CreateAction header to CategoryType list page
    ListCategoryTypes was missing getHeaderActions(), so no create button
    was shown in the admin panel.

--- laravel/app/Filament/Resources/CategoryTypeResource/Pages/ListCategoryTypes.php
+++ laravel/app/Filament/Resources/CategoryTypeResource/Pages/ListCategoryTypes.php
@@ -3,9 +3,17 @@
 namespace App\Filament\Resources\CategoryTypeResource\Pages;
 
 use App\Filament\Resources\CategoryTypeResource;
+use Filament\Actions;
 use Filament\Resources\Pages\ListRecords;
 
 class ListCategoryTypes extends ListRecords
 {
     protected static string $resource = CategoryTypeResource::class;
+
+    protected function getHeaderActions(): array
+    {
+        return [
+            Actions\CreateAction::make(),
+        ];
+    }
 }
